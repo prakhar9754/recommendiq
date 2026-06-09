af9c17b74e6a8543ffbe685e94729e6639768489
Prakhar Shrivastava <darshilshrivastava5@gmail.com>
2026-06-09 22:32:55 +0530

    database conection tested successfully

--- src/database.py
+++ src/database.py
@@ -40,10 +40,10 @@ def insert_users(
     """
 
     data = [
-        (user,)
+        (user,) #tuple row by row
         for user in users_df["visitorid"].unique()
     ]
-
+    #insert all rows at once 
     cursor.executemany(
         query,
         data
@@ -54,3 +54,91 @@ def insert_users(
     cursor.close()
 
     connection.close()
+    
+def insert_items(items_df: pd.DataFrame) -> None:
+    """
+    Insert unique items into items table.
+    """
+
+    connection = create_connection()
+
+    cursor = connection.cursor()
+
+    query = """
+    INSERT IGNORE INTO items(item_id)
+    VALUES (%s)
+    """
+
+    data = [
+        (item,)
+        for item in items_df["itemid"].unique()
+    ]
+
+    cursor.executemany(query, data)
+
+    connection.commit()
+
+    cursor.close()
+    connection.close()
+    
+def insert_interactions(
+    interaction_df: pd.DataFrame
+):
+    """
+    Insert engineered interactions.
+    """
+    connection = create_connection()
+    cursor = connection.cursor()
+    query = """
+     INSERT INTO interactions(
+        visitor_id,
+        item_id,
+        interaction_strength,
+        recency_days
+    )
+    VALUES (%s, %s, %s, %s)
+    """
+    data = list(
+        interaction_df[
+            [
+                "visitorid",
+                "itemid",
+                "interaction_strength",
+                "recency_days"
+            ]
+        ].itertuples(
+            index=False,
+            name=None
+        )
+    )
+    cursor.executemany(
+        query,
+        data
+    )
+
+    connection.commit()
+
+    cursor.close()
+
+    connection.close()
+    
+#Read Data Back
+def fetch_interactions():
+    """
+    Retrieve interactions.
+    """
+
+    connection = create_connection()
+
+    query = """
+    SELECT *
+    FROM interactions
+    """
+    
+    interactions_df = pd.read_sql(
+        query,
+        connection
+    )
+    connection.close()
+
+    return interactions_df

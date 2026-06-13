a5b056d9cd0fb9dc8daaf59b02eee5480f3fa0d1
Prakhar Shrivastava <darshilshrivastava5@gmail.com>
2026-06-13 17:20:06 +0530

    new structure updated and database probleum solved

--- src/database.py
+++ src/database.py
@@ -1,18 +1,15 @@
 from sqlalchemy import create_engine
+from sqlalchemy.engine import URL
 
-# Database Configuration
-DB_USER = "root"
-DB_PASSWORD = "Prakhar@12"
-DB_HOST = "localhost"
-DB_PORT = "3306"
-DB_NAME = "recommendation_engine"
 
 def get_engine():
-    """
-    Create and return SQLAlchemy engine.
-    """
-    engine = create_engine(
-        f"mysql+mysqlconnector://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+    connection_url = URL.create(
+        drivername="mysql+pymysql",
+        username="root",
+        password="Prakhar@12",
+        host="localhost",
+        database="recommendation_engine"
     )
 
-    return engine
+    return create_engine(connection_url)
